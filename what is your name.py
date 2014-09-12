2ff8a2c6a8547fd500aa7ef2f10cc198c9d1d92c
jackgoode123 <30508@longroad.ac.uk>
2014-09-12 15:46:49 +0100

    whats your name

--- what is your name.py
+++ what is your name.py
@@ -2,6 +2,6 @@
 #09-09-2014
 #what is your name
 
-first_name = input ("please enter your first name")
-print (frist_name)
-print ("hi {0}!".format(frist_name))
+first_name = input ("please enter your first name: ")
+print (first_name)
+print ("hi {0}!".format(first_name))
