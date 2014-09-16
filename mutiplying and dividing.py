0a25363bf8e357bc19c96855bb7285ad125b1a25
jackgoode123 <30508@longroad.ac.uk>
2014-09-16 10:55:06 +0100

    time and dividing

---
+++ mutiplying and dividing.py
@@ -0,0 +1,12 @@
+#jack goode
+#16-09-14
+#mutipling and dividing
+
+print("please pick three numbers")
+number1 = float(input("please enter: "))
+number2 = float(input("please enter: "))
+number3 = float(input("please enter: "))
+
+answer = (number1 + number2) * number3
+
+print(answer)
