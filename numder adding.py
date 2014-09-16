5ffce83b1b3603c683f626936c3b442177ed08c9
jackgoode123 <30508@longroad.ac.uk>
2014-09-16 10:27:00 +0100

    adding 2 numbers together

--- numder adding.py
+++ numder adding.py
@@ -2,8 +2,9 @@
 #12-09-14
 #numder adding
 
-numder1 = int(input("please enter numder: "))
-numder2 = int(input("please enter numder: "))
+numder1 = float(input("please enter numder: "))
+numder2 = float(input("please enter numder: "))
 
 answer = numder1 + numder2
 print(answer)
+
