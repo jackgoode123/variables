e23160a6ab669c041ec1d417d87a7d4ef178765f
jackgoode123 <30508@longroad.ac.uk>
2014-09-16 10:27:37 +0100

    adding 4 numbers

---
+++ adding 4 number together.py
@@ -0,0 +1,13 @@
+# jack goode
+# 16-09-14
+# adding 4 nubers together
+
+print("please enter four numbers between 1 and 10")
+
+number1 = float(input("first number: "))
+number2 = float(input("second number: "))
+number3 = float(input("thrid number: "))
+number4 = float(input("fourth number: "))
+
+answer = number1 + number2 + number3 + number4
+print(answer)
